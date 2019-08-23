d4612f100d379bd5ba8425dce900656c00df0100
Fredrick Barrett <f.barrett12@gmail.com>
2019-08-23 04:36:31 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(numbers)
   counter = 0 
   new_array = []
   while counter < numbers.length do 
-    numbers[counter] ** 2 << new_array
+    new_array << numbers[counter] ** 2
     counter += 1
   end
   return new_array
